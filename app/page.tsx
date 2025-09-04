562f8f8bb49a00bcde73aef9f800af30359b7bc2
Nodari Vadatchkoria <n.vadatchkoria@alte.edu.ge>
2025-09-04 09:27:28 +0400

    Move Message Templates section from left sidebar to right sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -102,15 +102,6 @@ export default function BroadcastPage() {
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-          {/* Left Sidebar - Templates and Personalization */}
-          <div className="lg:col-span-1 space-y-6">
-            <MessageTemplates
-              templates={mockMessageTemplates}
-              onTemplateSelect={handleTemplateSelect}
-            />
-            
-          </div>
-
           {/* Main Content */}
           <div className="lg:col-span-2 space-y-6">
             {/* Audience Selection */}
@@ -193,6 +184,14 @@ export default function BroadcastPage() {
                 </button>
               </div>
             </div>
+          </div>
+
+          {/* Right Sidebar - Message Templates */}
+          <div className="lg:col-span-1 space-y-6">
+            <MessageTemplates
+              templates={mockMessageTemplates}
+              onTemplateSelect={handleTemplateSelect}
+            />
           </div>
         </div>
       </div>
